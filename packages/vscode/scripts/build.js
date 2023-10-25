598149453a2cc9f15d50d276757f5bf672e8bd9c
Johnson Chu <johnsoncodehk@gmail.com>
2023-10-26 02:06:47 +0800

    chore: bump deps

--- packages/vscode/scripts/build.js
+++ packages/vscode/scripts/build.js
@@ -1,4 +1,4 @@
-require('esbuild').build({
+require("esbuild").context({
 	entryPoints: {
 		client: './src/extension.js',
 		server: './node_modules/@html1/language-server/bin/html1-language-server.js',
@@ -14,18 +14,25 @@ require('esbuild').build({
 	tsconfig: './tsconfig.json',
 	define: { 'process.env.NODE_ENV': '"production"' },
 	minify: process.argv.includes('--minify'),
-	watch: process.argv.includes('--watch'),
 	plugins: [
 		{
 			name: 'umd2esm',
 			setup(build) {
 				build.onResolve({ filter: /^(vscode-.*-languageservice|jsonc-parser)/ }, args => {
-					const pathUmdMay = require.resolve(args.path, { paths: [args.resolveDir] })
+					const pathUmdMay = require.resolve(args.path, { paths: [args.resolveDir] });
 					// Call twice the replace is to solve the problem of the path in Windows
-					const pathEsm = pathUmdMay.replace('/umd/', '/esm/').replace('\\umd\\', '\\esm\\')
-					return { path: pathEsm }
-				})
+					const pathEsm = pathUmdMay.replace('/umd/', '/esm/').replace('\\umd\\', '\\esm\\');
+					return { path: pathEsm };
+				});
 			},
 		},
 	],
-}).catch(() => process.exit(1))
+}).then(async ctx => {
+	if (process.argv.includes('--watch')) {
+		await ctx.watch();
+		console.log('watching...');
+	} else {
+		await ctx.rebuild();
+		await ctx.dispose();
+	}
+});
