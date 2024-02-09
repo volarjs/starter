e13dbe1a62c90278d33d0e683c332270a7500bca
Johnson Chu <johnsoncodehk@gmail.com>
2024-02-10 06:42:34 +0800

    chore: improve type conversion

--- packages/language-server/src/index.ts
+++ packages/language-server/src/index.ts
@@ -3,7 +3,7 @@ import { create as createEmmetService } from 'volar-service-emmet';
 import { create as createHtmlService } from 'volar-service-html';
 import { create as createCssService } from 'volar-service-css';
 import { create as createTypeScriptService } from 'volar-service-typescript';
-import { createServer, createConnection, createTypeScriptProjectProvider, Diagnostic } from '@volar/language-server/node';
+import { createServer, createConnection, createTypeScriptProjectProvider, Diagnostic, VirtualCode } from '@volar/language-server/node';
 
 const connection = createConnection();
 const server = createServer(connection);
@@ -25,14 +25,14 @@ connection.onInitialize(params => {
 					create(context) {
 						return {
 							provideDiagnostics(document) {
-
-								const [virtualCode] = context.documents.getVirtualCodeByUri(document.uri);
-								if (virtualCode && 'htmlDocument' in virtualCode) return;
-
-								const html1Code = virtualCode as Html1Code;
-								const styleNodes = html1Code.htmlDocument.roots.filter(root => root.tag === 'style');
-								if (styleNodes.length <= 1) return;
-
+								const virtualCode = context.documents.getVirtualCodeByUri(document.uri)[0] as VirtualCode | Html1Code | undefined;
+								if (!virtualCode || !('htmlDocument' in virtualCode)) {
+									return;
+								}
+								const styleNodes = virtualCode.htmlDocument.roots.filter(root => root.tag === 'style');
+								if (styleNodes.length <= 1) {
+									return;
+								}
 								const errors: Diagnostic[] = [];
 								for (let i = 1; i < styleNodes.length; i++) {
 									errors.push({
